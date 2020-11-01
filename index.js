b9f636bc947de78aebcd3631f2e9c970f840a708
t3ssk <te.konecna@icloud.com>
2020-11-01 17:41:59 +0100

    finish index.html

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ const menu = document.querySelector('.menu')
 const dropdown = document.querySelector('.navigace')
 const close = document.querySelector('.close')
 
-menu.addEventListener('click', ()=>{dropdown.classList.add('active')})
-close.addEventListener('click', ()=>{dropdown.classList.remove('active')})
+menu.addEventListener('click', ()=>{dropdown.classList.add('active'); document.querySelector('main').classList.add('inactive');document.querySelector('footer').classList.add('inactive');})
+close.addEventListener('click', ()=>{dropdown.classList.remove('active');document.querySelector('main').classList.remove('inactive');document.querySelector('footer').classList.remove('inactive');})
+
+import LocomotiveScroll from 'locomotive-scroll';
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    smoothMobile: true,
+    getSpeed: true,
+    getDirection: true,
+    useKeyboard: true
+});
